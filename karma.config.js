b823445c49d8aa70760a9cc21ab2c99c2f949868
Eugene Safronov <eugene.safronov@auctionata.com>
2015-10-01 23:28:29 +0200

    added app/node_modules back into sources

--- karma.config.js
+++ karma.config.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/angular/angular.js',
+      'app/node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
 
        'tests/*.js',
